b5626d0fa24615e62882b88d6f074beb33fb1f9f
root <root@localhost.localdomain>
2014-06-27 14:22:11 +0900

    ......

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-
 var thrift = require('thrift');
-// var thriftTransports = thrift.TBufferedTransport();
-// var thriftProtocols = require('thrift/protocol');
-
-
-
-var scribe = require('./scribe');
-var scribeTypes = require('./scribe_types');
+var datasource = require('./datasource');
+var datasourceTypes = require('./datasource_types');
 
 var transport = thrift.TBufferedTransport();
 var protocol = thrift.TBinaryProtocol();
@@ -15,24 +9,21 @@ var connection = thrift.createConnection("localhost", 8080,{
 	,protocol: protocol
 });
 
-connection.on('error', function(err) {  
-   console.error(err);                   
-});                                     
-
-var client = thrift.createClient(scribe, connection);
-
-var message = new scribeTypes.LogEntry({
-	category: "hello_category"
-	,message: "hello"}
-);
-var messages = [message];
-
-client.Log(messages, function(err, message) {
-	console.log(err, message);
-	connection.end();
+connection.on('error', function(err) {
+	console.error(err);
 });
 
+var client = thrift.createClient(datasource, connection);
 
+var message = new datasourceTypes.Entry({
+	classname: "testclassname2",
+	methodname: "methodname"
+});
 
-
+client.query(message, function(err, m) {
+	if(null === err){
+		console.log(m);
+	}
+	connection.end();
+});
 
